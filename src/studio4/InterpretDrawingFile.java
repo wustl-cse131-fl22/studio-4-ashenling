69448902cc9034b8245e23e14ec3567cdd63dc22
qingtan <qingtan@tracys-mbp.dhcp.wustl.edu>
2022-10-13 12:46:21 -0500

    Studio 4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -18,7 +19,33 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
+	
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
 		
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean filled = in.nextBoolean();
+		int n=in.nextInt();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		double array[]=new double[n/2];
+		double array2[]=new double[n/2];
+		
+		for(int i=0; i<n;i++) {
+		 array[i]=in.nextDouble();
+		}
+		for(int i=0;i<n;i++) {
+			array2[i]=in.nextDouble();
+		}
+		if (filled = true) {
+			if (shapeType == "triangle") {
+			StdDraw.setPenColor(StdDraw.PINK);
+			StdDraw.filledPolygon(array,array2);
+			}
+		}
 	}
 }
